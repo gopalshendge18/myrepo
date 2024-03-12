0d221fe2fac69227c4e3d1eb369e8863b9e4f391
gopalshendge18 <85784714+gopalshendge18@users.noreply.github.com>
2024-03-12 09:38:42 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,9 +1,12 @@
 from ultralytics import YOLO
 
-model = YOLO("best (2).pt")
+model = YOLO("best (1).pt")
 
+# Perform object detection on the image
+results = model.predict("C:\\Users\\Rahul -hp\\Desktop\\Internship\\TRDDC\\Internship 2\\Logical_07000.jpeg", save=True, save_txt=True)
+for result in results:
+    boxes = result.boxes
 
-results =  model.predict("C:\\Users\\Rahul -hp\\Desktop\\Internship\\TRDDC\\Internship 2\\Logical_07000.jpeg", save=True)
-
-
+print(boxes.xyxy)
+# Extract bounding box coordinates of all detected objects
 
