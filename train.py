28d4dfee9e816d2e5d991886b6f7198ac47287cd
gopalshendge18 <85784714+gopalshendge18@users.noreply.github.com>
2024-03-13 16:42:02 +0530

    Update train.py

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 import cv2
 model = YOLO("best (1).pt")
 import numpy
-
+import pytesseract as py
 results = model.predict("C:\\Users\\2707109\\Desktop\\project_yolo\\class2.v1i.yolov8 (1)\\Logical_07200.jpeg", save=True)
 print(type(results))
 print(results)
@@ -13,7 +13,7 @@ boxes= boxes.numpy()
 print(boxes.xyxy)
 
 image=  cv2.imread("C:\\Users\\2707109\\Desktop\\project_yolo\\class2.v1i.yolov8 (1)\\Logical_07200.jpeg")
-
+py.pytesseract.tesseract_cmd = r'C:\Users\2707109\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
 window_name=  'image'
 # from PIL import Image
 
@@ -22,18 +22,26 @@ window_name=  'image'
 # cv2.waitKey(0)
 print("Shape of the image", image.shape) 
 temp =0
-# for index in boxes.xywh:
+A= []
+for index in boxes.xyxy:
     
-#     temp+=1
-#     x = round(index[0])
-#     y = round(index[1])
-#     w = round(index[2])
-#     h = round(index[3])
-#     print("x, y ,w ,h\n",index[0],index[1],index[2],index[3])
-x,y,w,h= {1167 ,     1685 ,    1429  ,      1859}
-h= 1859-1685
-w= 1429-1167
-crop_img = image[y:y+h, x:x+w]
-cv2.imwrite("Cropped_img/crop__" + str(temp) + ".jpg",crop_img)
+    temp+=1
+    x = int(index[0])
+    y = round(index[1])
+    w = round(index[2])
+    h = round(index[3])
+    print("x, y ,w ,h\n",index[0],index[1],index[2],index[3])
+# x,y,w,h= [1167 ,     1685 ,    1429  ,      1859]
+    h= h-y
+    w= w-x
+    print("h, w",x,y)
+    crop_img = image[y:y+h, x:x+w]
+# cv2.imshow(window_name, crop_img)
+    cv2.imwrite("Cropped_img/crop__" + str(temp) + ".jpg",crop_img)
+  
+    #passing to tesseract
 
+    c = py.image_to_string(crop_img)
+    A.append(c)
+print(A)
 
